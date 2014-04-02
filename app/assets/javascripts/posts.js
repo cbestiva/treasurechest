9a5a62129270af69fbe097b789a7d8eaccbda493
cbestiva <cbestiva@gmail.com>
2014-04-01 17:40:27 -0700

    added geobytes api for post city

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -7,4 +7,19 @@ $(document).ready(function() {
       itemSelector: '.item'
     });
   });
+
+  jQuery(function () {
+   jQuery("#post_city").autocomplete({
+    source: function (request, response) {
+     jQuery.getJSON(
+      "http://gd.geobytes.com/AutoCompleteCity?callback=?&filter=US&q="+request.term,
+      function (data) {
+       response(data);
+      }
+     );
+    },
+   });
+   jQuery("#post_city").autocomplete("option", "delay", 100);
+  });
+
 });
